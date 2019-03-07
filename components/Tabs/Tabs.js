3bd7e55dd218dddb90751e1fb8c6bdf35fe1cc31
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-07 15:12:20 -0500

    finished TabLink class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,16 +2,15 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+     this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
-    
+     this.data = this.element.dataset.tab
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    
+     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.element.dataset.tab}']`); ;
+    //console.log(this.itemElement)
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+     this.tabItem = new TabItem(this.tabs-item);
     
     // Add a click event listener on this instance, calling the select method on click
 
@@ -49,14 +48,14 @@ class TabItem {
   }
 }
 
-/* START HERE: 
+//  START HERE: 
 
-- Select all classes named ".tabs-link" and assign that value to the links variable
+// - Select all classes named ".tabs-link" and assign that value to the links variable
+links = document.querySelectorAll('.tabs-link')
 
-- With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
-
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
-
-*/
-
-links = document.querySelectorAll();
+// - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
+links.forEach(function(element){
+  return new TabLink(element)
+});
+ 
+// - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
