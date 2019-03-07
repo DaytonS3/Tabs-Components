864f75d0fdb9a2bea1b8cb53ce14de25176fd034
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-07 17:13:08 -0500

    MVP final

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,12 +19,13 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-     const links = document.querySelectorAll('.tabs-links');
-
+     const links = document.querySelectorAll('.tabs-link');
+    
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-     Array.from(links).forEach(link => {
-       //console.log(links)
+     links.forEach(link => {
+       console.log(link)
         link.classList.remove('tabs-link-selected')
+        
      });
      
     // Add a class named "tabs-link-selected" to this link
@@ -46,9 +47,8 @@ class TabItem {
      const items = document.querySelectorAll('.tabs-item');
      
     // Remove the class "tabs-item-selected" from each element
-    Array.from(items).forEach(item => {
-      item.classList.remove('tabs-item-selected')
-     });
+    Array.from(items).forEach(item => item.classList.remove('tabs-item-selected'));
+     
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
   }
